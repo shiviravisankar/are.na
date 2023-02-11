8fa1ee24597113222a856ffe44a34e97b3c548ef
shiviravisankar <shiviravis@gmail.com>
2023-02-11 18:21:47 -0500

    scroll effect

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
+  {
     document.getElementById("header").style.fontSize = "15vw";
     document.getElementById("header").style.lineHeight = "13vw";
-  } else {
+  } 
+  else {
     document.getElementById("header").style.fontSize = "60vw";
     document.getElementById("header").style.lineHeight = "50vw";
   }
