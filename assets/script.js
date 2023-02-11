f472d8e0ace9a3c28bd4bca825953eec95d34b74
shiviravisankar <shiviravis@gmail.com>
2023-02-11 16:33:09 -0500

    updated

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("heading").style.fontSize = "30px";
+  } else {
+    document.getElementById("heading").style.fontSize = "90px";
+  }
+}
