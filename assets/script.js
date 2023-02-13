1a23b0fbc2ae415de39276cd5b04c3127e7f4f17
shiviravisankar <shiviravis@gmail.com>
2023-02-13 14:40:04 -0500

    static implementation

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,1 @@
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
-  {
-    document.getElementById("header").style.fontSize = "15vw";
-    document.getElementById("header").style.lineHeight = "13vw";
-  } 
-  else {
-    document.getElementById("header").style.fontSize = "60vw";
-    document.getElementById("header").style.lineHeight = "50vw";
-  }
-}
