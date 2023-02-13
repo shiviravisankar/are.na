faf4561f2cf694b3d1f3a9e43efd8f7692349740
shiviravisankar <shiviravis@gmail.com>
2023-02-13 16:29:01 -0500

    added elements

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
+  {
+    document.getElementById("header").style.fontSize = "15vw";
+    document.getElementById("header").style.lineHeight = "13vw";
+  } 
+  else {
+    document.getElementById("header").style.fontSize = "50vw";
+    document.getElementById("header").style.lineHeight = "45vw";
+  }
+}
