97badf45ae172a24bd44eb10a26d0a678eff4791
shiviravisankar <shiviravis@gmail.com>
2023-02-28 14:20:50 -0500

    added id to html's

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,15 @@
 window.arenaCallback = () => {
 	// Put your JavaScript in here!
 	// This will run after Are.na’s API returns your data.
+
+	// parse data to extract what you want to filter
+    const channelDescription = data.items;
+
+    // filter function
+    const filteredItems = channelDescription.filter(item => item.category === 'fear');
+
+    // display filtered results on different HTML pages
+    const filteredResultsPage1 = document.querySelector('#index.html');
+    const filteredResultsPage2 = document.querySelector('#page2');
+
 }
