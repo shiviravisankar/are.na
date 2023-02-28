e4c5089d34924f8ebae473717c39decedf6cd04d
shiviravisankar <shiviravis@gmail.com>
2023-02-28 17:02:16 -0500

    code added to main.js

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,15 @@
 window.arenaCallback = () => {
 
+const content = document.createElement('div');
+    content.id = 'content';
+    document.body.appendChild(content);
+
+const filteredData = data.filter(item => item.description.includes('fear'));
+
+filteredData.forEach(item => {
+  const block = document.createElement('div');
+  block.innerHTML = item.description;
+  content.appendChild(block);
+});
 
 }
